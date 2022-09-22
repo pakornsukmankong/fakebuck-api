d4fe421b7f6577ca62d9a2f9f523780d1737a756
pakornsukmankong <floksong.001@gmail.com>
2022-09-22 14:45:47 +0700

    feature update user

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const authRoute = require('./routes/authRoute');
+const userRoute = require('./routes/userRoute');
 const notFound = require('./middlewares/notFound');
 const error = require('./middlewares/error');
 
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/auth', authRoute);
+app.use('/users', userRoute);
 
 app.use(notFound);
 app.use(error);
