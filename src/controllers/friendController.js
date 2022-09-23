c65d5be094ccf13fb940f980c7ebfbf4f66671aa
pakornsukmankong <floksong.001@gmail.com>
2022-09-23 15:07:47 +0700

    feature add friend

--- src/controllers/friendController.js
+++ src/controllers/friendController.js
@@ -1,5 +1,7 @@
 const { Op } = require('sequelize');
+const { FRIEND_PENDING, FRIEND_ACCEPTED } = require('../config/constants');
 const { Friend } = require('../models');
+const AppError = require('../utils/appError');
 
 exports.deleteFriend = async (req, res, next) => {
   try {
@@ -19,6 +21,58 @@ exports.deleteFriend = async (req, res, next) => {
     });
 
     res.status(204).json();
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.createFriend = async (req, res, next) => {
+  try {
+    const { friendId } = req.params;
+    const existFriend = await Friend.findOne({
+      where: {
+        [Op.or]: [
+          {
+            requesterId: req.user.id,
+            accepterId: friendId,
+          },
+          { requesterId: friendId, accepterId: req.user.id },
+        ],
+      },
+    });
+
+    if (existFriend) {
+      throw new AppError('already friends or pending', 400);
+    }
+
+    await Friend.create({
+      status: FRIEND_PENDING,
+      requesterId: req.user.id,
+      accepterId: friendId,
+    });
+
+    res.status(200).json({ message: 'success request friend' });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateFriend = async (req, res, next) => {
+  try {
+    const { friendId } = req.params;
+    //SELECT * FROM friends WHERE status = "PENDING" AND accepter_id = req.user.id
+    //AND requester_id = friendId
+    await Friend.update(
+      { status: FRIEND_ACCEPTED },
+      {
+        where: {
+          status: FRIEND_PENDING,
+          requesterId: friendId,
+          accepterId: req.user.id,
+        },
+      }
+    );
+    res.status(200).json({ message: 'success add friend' });
   } catch (err) {
     next(err);
   }
