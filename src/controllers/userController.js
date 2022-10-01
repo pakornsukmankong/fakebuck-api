3a2f5ef33b4ee4d0f2719e4e9b4d97fb1c7639e4
pakornsukmankong <floksong.001@gmail.com>
2022-10-01 15:10:08 +0700

    rearrage code in userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,64 +1,64 @@
-const fs = require('fs');
-const cloudinary = require('../utils/cloudinary');
-const { User } = require('../models');
-const AppError = require('../utils/appError');
-const friendService = require('../services/friendService');
+const fs = require('fs')
+const cloudinary = require('../utils/cloudinary')
+const { User } = require('../models')
+const AppError = require('../utils/appError')
+const friendService = require('../services/friendService')
 
 exports.updateUser = async (req, res, next) => {
   try {
-    const { password, ...updateValue } = req.body;
+    const { password, ...updateValue } = req.body
 
     if (req.files.profileImage) {
-      const profileImage = req.user.profileImage;
+      const profileImage = req.user.profileImage
 
       const secureUrl = await cloudinary.upload(
         req.files.profileImage[0].path,
         profileImage ? cloudinary.getPublicId(profileImage) : undefined
-      );
+      )
 
-      updateValue.profileImage = secureUrl;
-      fs.unlinkSync(req.files.profileImage[0].path);
+      updateValue.profileImage = secureUrl
+      fs.unlinkSync(req.files.profileImage[0].path)
     }
     if (req.files.coverImage) {
-      const coverImage = req.user.coverImage;
+      const coverImage = req.user.coverImage
 
       const secure_url = await cloudinary.upload(
         req.files.coverImage[0].path,
         coverImage ? cloudinary.getPublicId(coverImage) : undefined
-      );
-      updateValue.coverImage = secure_url;
-      fs.unlinkSync(req.files.coverImage[0].path);
+      )
+      updateValue.coverImage = secure_url
+      fs.unlinkSync(req.files.coverImage[0].path)
     }
 
-    await User.update(updateValue, { where: { id: req.user.id } });
+    await User.update(updateValue, { where: { id: req.user.id } })
 
     const user = await User.findOne({
       where: { id: req.user.id },
       attributes: { exclude: 'password' },
-    });
+    })
 
-    res.status(200).json({ user });
+    res.status(200).json({ user })
   } catch (err) {
-    next(err);
+    next(err)
   }
-};
+}
 
 exports.getUserFriends = async (req, res, next) => {
   try {
-    const id = +req.params.id;
+    const id = +req.params.id
     const user = await User.findOne({
       where: { id },
       attributes: { exclude: 'password' },
-    });
+    })
 
     if (!user) {
-      throw new AppError('user not found', 400);
+      throw new AppError('user not found', 400)
     }
 
-    const friends = await friendService.findUserFriendsByUserId(id);
-    const statusWithMe = await friendService.findStatusWithMe(req.user.id, id);
-    res.status(200).json({ user, friends, statusWithMe });
+    const friends = await friendService.findUserFriendsByUserId(id)
+    const statusWithMe = await friendService.findStatusWithMe(req.user.id, id)
+    res.status(200).json({ user, friends, statusWithMe })
   } catch (err) {
-    next(err);
+    next(err)
   }
-};
+}
